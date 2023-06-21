02521777c46fbfb7b48e5e2cb92afeb4da32d786
Vassilis Virvilis <vasvir2@gmail.com>
2023-06-21 17:20:58 +0300

    tone down the logging

--- scan.js
+++ scan.js
@@ -3,7 +3,7 @@
 //console.log("scanPageJs for " + location);
 var __gwt_activeModules2 = {};
 if (typeof __gwt_activeModules !== 'undefined') {
-    console.log(__gwt_activeModules);
+    //console.log(__gwt_activeModules);
     for (const k in __gwt_activeModules) {
         __gwt_activeModules2[k] = 'superdevmode' in __gwt_activeModules[k] ? true : false;
     }
